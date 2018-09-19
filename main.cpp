998f0c8153f87a06a0b458e67a3caf9f93a25673
royeking7 <rking27@mail.csuchico.edu>
2018-09-19 16:35:48 -0700

    Asks user for first and last name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Roy King
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,14 @@ using namespace std;
 int main()
 {
   //declare variables
-
+  string first;
+  string last;
   //get user input
-
+  cout<<" Hello, whats your first name? " <<first<< endl;
+  cin>>first;
+  
+  cout<<" What is your last name? " <<last<< endl;
+  cin>>last;
   //tell fortune
 
   return 0;
