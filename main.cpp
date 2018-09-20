fd57602ef7cda99e2243dcd78ddd8fbbf158f1f6
royeking7 <rking27@mail.csuchico.edu>
2018-09-19 17:20:47 -0700

    Tells your fortune

--- main.cpp
+++ main.cpp
@@ -9,13 +9,17 @@ int main()
   //declare variables
   string first;
   string last;
+  
   //get user input
-  cout<<" Hello, whats your first name? " <<first<< endl;
+  cout<<"Hello, whats your first name? " <<first<< endl;
   cin>>first;
   
-  cout<<" What is your last name? " <<last<< endl;
+  cout<<"What is your last name? " <<last<< endl;
   cin>>last;
+
   //tell fortune
+  cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"."<<endl; 
+  cout<<"Your lucky number is "<<first.length()<<endl;
 
   return 0;
 }
