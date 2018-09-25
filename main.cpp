be95750801ca6ab71af0613e149b41f46f9633e0
royeking7 <rking27@mail.csuchico.edu>
2018-09-24 21:04:53 -0700

    lazyHoroscope

--- main.cpp
+++ main.cpp
@@ -15,15 +15,15 @@ int main()
   string last;
   
   //get user input
-  cout<<"Hello, whats your first name? " <<first<< endl;
+  cout<<"Whats your first name? " <<first<< endl;
   cin>>first;
   
   cout<<"What is your last name? " <<last<< endl;
   cin>>last;
 
   //tell fortune
-  cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"."<<endl; 
-  cout<<"Your lucky number is "<<first.length()<<endl;
+  cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune..."<<endl; 
+  cout<<"your lucky number is "<<first.length()<<endl;
   
   //getting vowels
   index=first[0];
@@ -31,20 +31,20 @@ int main()
   
   if(index=='a'||index=='A'||index=='e'||index=='E'||index=='i'||index=='I'||index=='o'||index=='O'||index=='u'||index=='U') 
   {  
-  cout<<"You are destined to be famous"<<endl;
+  cout<<"you are destined to be famous"<<endl;
   }
   else
   {
-  cout<<"You should keep a low profile"<<endl;
+  cout<<"you should keep a low profile"<<endl;
   }
   
   if(index2=='a'||index2=='A'||index2=='e'||index2=='E'||index2=='i'||index2=='I'||index2=='o'||index2=='O'||index2=='u'||index2=='U')
   {
-  cout<<"You have already met your true love"<<endl;
+  cout<<"you have already met your true love"<<endl;
   }
   
   
-  cout<<"Have a good day!"<<endl;
+  cout<<"have a good day!"<<endl;
   
   return 0;
 }  
